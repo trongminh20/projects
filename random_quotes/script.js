f25559917ed64a09ad6eb17c9407ba47d4fac5c9
minh phan <trongminh20@gmail.com>
2018-04-23 13:10:44 -0700

    random quote

--- random_quotes/script.js
+++ random_quotes/script.js
@@ -1,15 +1,34 @@
+var quote = "";
+var au = "";
+
+
 function random_quote (){
-  var quotes = ['a','b','c','d'];
-  var random_number = Math.floor(Math.random()*quotes.length);
 
-    //return quotes[random_number];
+var quotes = ['Get busy living or get busy dying.', 'The first step toward success is taken when you refuse to be a captive of the environment in which you first find yourself', 'When one door of happiness closes, another opens; but often we look so long at the closed door that we do not see the one which has been opened for us.'];
 
-    //return document.write(quotes[random_number]);
 
-    return document.getElementById('showing').innerHTML = quotes[random_number];
+var author = ['Stephen King','Mark Caine','Helen Keller'];
+
+  var i = Math.floor(Math.random()*quotes.length);
+
+  quote = quotes[i];
+  au = author[i];
+    document.getElementById('content').innerHTML = '"'+quotes[i]+'"';
+
+    document.getElementById('author').innerHTML = "--" + author[i] +'--';	
   }
 
 
-  //console.log(random_quote());
+ function tweet() {
+      window.open(
+        "https://twitter.com/intent/tweet?text=" +
+          '"' +
+           quote+
+          '"' +
+          "-" +
+          au +
+          "-"
+      );
+    };
 
-  document.getElementById('btn').addEventListener('click', random_quote());
+ 
