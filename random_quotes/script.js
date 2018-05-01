40ae29ef94ba869a1fa93e018a7f4d21f8a9de7e
minh phan <trongminh20@gmail.com>
2018-04-30 23:28:23 -0700

    update randomquote

--- random_quotes/script.js
+++ random_quotes/script.js
@@ -1,33 +1,29 @@
-var quote = "";
-var au = "";
+function quotes_source(quotes, author){
+  this.quotes = quotes;
+  this.author = author;
 
+  this.random = function(){
+    var i = Math.floor(Math.random()*quotes.length);
 
-function random_quote (){
-
-var quotes = ['Get busy living or get busy dying.', 'The first step toward success is taken when you refuse to be a captive of the environment in which you first find yourself', 'When one door of happiness closes, another opens; but often we look so long at the closed door that we do not see the one which has been opened for us.'];
-
-
-var author = ['Stephen King','Mark Caine','Helen Keller'];
-
-  var i = Math.floor(Math.random()*quotes.length);
-
-  quote = quotes[i];
-  au = author[i];
-    document.getElementById('content').innerHTML = '"'+quotes[i]+'"';
-
-    document.getElementById('author').innerHTML = "--" + author[i] +'--';	
+    return quotes[i] + '<br/>' + '--'+author[i]+'--';
   }
+}
+
+var quo = new quotes_source(['Get busy living or get busy dying.', 'The first step toward success is taken when you refuse to be a captive of the environment in which you first find yourself', 'When one door of happiness closes, another opens; but often we look so long at the closed door that we do not see the one which has been opened for us.'], ['Stephen King','Mark Caine','Helen Keller']);
+
+//console.log(quo.random());
+
+function click_event(){
+  document.getElementById('content').innerHTML = quo.random();
+}
 
 
  function tweet() {
       window.open(
         "https://twitter.com/intent/tweet?text=" +
           '"' +
-           quote+
-          '"' +
-          "-" +
-          au +
-          "-"
+           quo.random()+
+          '"' 
       );
     };
 
